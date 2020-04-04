d247718f44a44fb0696e521a337e0506085605a7
junx1992 <junx1992@gmail.com>
2020-04-04 20:12:34 +0800

    change files

--- c.py
+++ c.py
@@ -10,4 +10,6 @@ register_team = db.video
 a = register_team.find()
 import hashlib
 for item in a:
-	print (item['_id'], item['teamname'], item['caption_name'], item['caption_email'], item['caption_organization'], item['password'],)
+	print (item['_id'], item['teamname'])
+	for member in item['member']:
+		print(member['name'], member['email'], member['organization'])
